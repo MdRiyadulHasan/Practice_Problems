1176c05fe8f8c7fcaf97f05c965177bb30fa01e7
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-01-06 01:28:57 +0600

    Update 159_subarray_2_chars.py

--- Medium/159_subarray_2_chars.py
+++ Medium/159_subarray_2_chars.py
@@ -2,26 +2,20 @@ def lengthOfLongestSubstringTwoDistinct( s: str) -> int:
     ln = len(s)
     if ln == 0:
         return 0
-    
-    # to hold chars from the s so that I know that I have only two distinct characters
     dct = {}
     j = 0
     maxWindow = 0
     count = 0
     for i in range(ln):
-        dct[s[i]] = dct.get(s[i], 0) + 1
-        
+        dct[s[i]] = dct.get(s[i], 0) + 1      
         if dct[s[i]] == 1:
-            count += 1
-        
+            count += 1      
         while count > 2:
             dct[s[j]] -= 1
             if dct[s[j]] == 0:
                 count -= 1
-            j += 1
-        
+            j += 1       
         maxWindow = max(maxWindow, i - j + 1)
-
     return maxWindow
 if __name__ == '__main__':
     s='ccaabbb'
