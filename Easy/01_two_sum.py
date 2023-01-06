cf563cf4d9e83d8d89ece8065071de24173144c8
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-01-06 18:20:05 +0600

    Update 01_two_sum.py

--- Easy/01_two_sum.py
+++ Easy/01_two_sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def twoSum(self, nums, target: int):
         hasmap = {}
-        for i in range(len(nums)):
-            diff = target-nums[i]
+        for i,n in enumerate(nums):
+            diff=target-n
             if diff in hasmap:
-                return[hasmap[diff],i]
-            hasmap[nums[i]]=i
+                return [hasmap[diff],i]
+            hasmap[n]=i
 if __name__=='__main__':
     nums = [2,5,7,11,15]
     target = 9
     ob = Solution()
     ans = ob.twoSum(nums,target)
+    print(ans)
